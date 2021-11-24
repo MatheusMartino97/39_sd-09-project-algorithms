29cfe5188b04318c45c96513daa80aefe7a66d81
Matheus Martino <matheus.martino97@gmail.com>
2021-11-24 10:41:29 -0300

    Fix lint issues.

--- helpers/find_duplicate_helper.py
+++ helpers/find_duplicate_helper.py
@@ -1,8 +1,8 @@
-def sanitize_list_to_single_value (list):
-  if (len(list) == 1):
-    return list[0]
-  
-  if (len(list) == 0):
-    return False
+def sanitize_list_to_single_value(list):
+    if (len(list) == 1):
+      return list[0]
 
-  return list
+    if (len(list) == 0):
+      return False
+
+    return list
