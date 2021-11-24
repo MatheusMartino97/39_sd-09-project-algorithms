3c4b47154914a0db45c1906c003daef18ca3a4bc
Matheus Martino <matheus.martino97@gmail.com>
2021-11-24 10:51:30 -0300

    Fix lint issues.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -4,12 +4,12 @@ from helpers.find_duplicate_helper import sanitize_list_to_single_value
 def find_duplicate(nums):
     numbers_set = []
     duplicated_numbers = []
+    
+    if len(nums) <= 1:
+        return False
 
     for num in nums:
         if isinstance(num, str) or num < 0:
-            return False
-
-        if len(nums) <= 1:
             return False
 
         if num not in numbers_set:
