78d2d4fa4a0b08e2315da6054216e5fd710fe7d1
Matheus Martino <matheus.martino97@gmail.com>
2021-11-24 10:36:09 -0300

    Req 4.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,20 @@
+from helpers.find_duplicate_helper import sanitize_list_to_single_value
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    numbers_set = []
+    duplicated_numbers = []
+
+    for num in nums:
+        if isinstance(num, str) or num < 0:
+            return False
+        
+        if len(nums) <= 1:
+            return False
+
+        if num not in numbers_set:
+            numbers_set.append(num)
+        elif num not in duplicated_numbers:
+            duplicated_numbers.append(num)
+
+    return sanitize_list_to_single_value(duplicated_numbers)
