21f0087e5b8f1fb6aee542501d7b699c24f7f7df
Matheus Martino <matheus.martino97@gmail.com>
2021-11-24 09:08:44 -0300

    Req 1.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) is not int:
+        return None
+    
+    student_online_by_target_time_count = 0
+    
+    for (login_time, logout_time) in permanence_period:
+        if type(login_time) is not int or type(logout_time) is not int:
+            return None
+        
+        if target_time in range(login_time, logout_time + 1):
+            student_online_by_target_time_count += 1
+    
+    return student_online_by_target_time_count
