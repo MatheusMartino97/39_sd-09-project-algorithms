90a3028d208f4016ee50d3207d3b0518c72b3d74
Matheus Martino <matheus.martino97@gmail.com>
2021-11-24 09:56:51 -0300

    Fix lint issues.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,12 +3,11 @@ from helpers.anagrams_helper import sort_word
 def is_anagram(first_string, second_string):
     if not first_string or not second_string:
         return False
-    
+
     if len(first_string) != len(second_string):
         return False
-    
-    
+
     if sort_word(first_string) == sort_word(second_string):
         return True
-    
+
     return False
