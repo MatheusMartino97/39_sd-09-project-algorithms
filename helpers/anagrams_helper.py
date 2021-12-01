63309fecf42b8e1a45453c42ecadf2c9257ede2f
Matheus Martino <matheus.martino97@gmail.com>
2021-12-01 16:43:23 -0300

    Fix sort alg.

--- helpers/anagrams_helper.py
+++ helpers/anagrams_helper.py
@@ -1,17 +1,18 @@
 def sort_word(word):
+    array = [letter for letter in word]
     has_swapped = True
     num_of_iterations = 0
 
     while has_swapped:
         has_swapped = False
 
-        for i in range(len(word) - num_of_iterations - 1):
-            if word[i] > word[i + 1]:
-                word[i], word[i + 1] = word[i + 1], word[i]
+        for i in range(len(array) - num_of_iterations - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
                 has_swapped = True
         num_of_iterations += 1
 
-    return word
+    return array
 
 
 # Baseado no bubble sort do course Bloco 35 - Algoritmos
